cd1d1e59c3a66eb42664a5ee74c069603e1dac33
Alexander Shaposhnikov a.shaposhnikov@hh.ru <a.shaposhnikov@hh.ru>
2014-01-22 17:34:52 +0400

    clean up, fix name and description

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
-# clojure-hadoop
+# cascalog-wordcount
 
-A Clojure library designed to ... well, that part is up to you.
+example of cascalog usage
 
 ## Usage
 
-FIXME
+lein uberjar
+
+hadoop jar your-jar.jar *input* *output*
 
 ## License
 
-Copyright © 2013 FIXME
+Copyright © 2013 Alexander SHaposhnikov
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
--- project.clj
+++ project.clj
@@ -1,31 +1,21 @@
-(defproject clojure-hadoop "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject cascalog-wordcount "0.1.0-SNAPSHOT"
+  :description "minimal working cascalog example"
+  :url "https://github.com/hamilyon/cascalog-wordcount"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.5.1"]                                     
-
-                 [cascalog "2.0.0"]                                                 
-                 [backtype/dfs-datastores "1.1.2"]                                  
-                 [org.apache.thrift/libthrift "0.8.0"                               
-                  :exclusions [org.slf4j/slf4j-api]]]
-
-                 ;[cascalog/cascalog-core "2.0.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [cascalog "2.0.0"]
+                 [backtype/dfs-datastores "1.1.2"]]
 
   :jvm-opts ["-Xms768m" "-Xmx768m"]
   :source-paths ["src"]
 
-;  :profiles { :dev {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]]}}
-
   :profiles {
-    :provided
-        {
-            :dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
-  ]
-
-        }}
-;  :aot :all-with-unused
+    :provided {
+        :dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]]
+     }
+   }
   :main wc.core
 
   :uberjar-name "your-jar.jar"
